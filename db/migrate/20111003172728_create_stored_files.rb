422e0827bddf895850e36c3269a39f368f3a3797
Steph Skardal <steph@endpoint.com>
2011-10-10 10:37:52 -0400

    Minor migration update per new specs.

--- db/migrate/20111003172728_create_stored_files.rb
+++ db/migrate/20111003172728_create_stored_files.rb
@@ -3,12 +3,13 @@ class CreateStoredFiles < ActiveRecord::Migration
     create_table :stored_files do |t|
       t.references :batch_id
       t.references :user, :null => false
+      t.string :title
       t.string :original_file_name, :null => false
       t.string :collection_name
+      t.string :office
+      t.string :disposition
       t.references :access_level, :null => false
       t.references :content_type, :null => false
-      t.date :retention_plan_date
-      t.string :retention_plan_action
       t.string :format_name
       t.string :format_version
       t.string :mime_type
