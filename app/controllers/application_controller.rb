0e169799cc946e3c16e7cf2bb68d42b207ef01f7
Steph Skardal <steph@endpoint.com>
2011-10-31 09:33:10 -0400

    Added global rescue_from Acl9 AccessDenied exception, added error to layout.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  rescue_from Acl9::AccessDenied  do |exception|
+    flash[:error] = 'You do not have access to view this page.'
+    redirect_to root_url
+  end
 end
