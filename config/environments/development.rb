e6a3b993dc4fe35d50e1a784115b1f587f58e760
Brian Buchalter <brianb@endpoint.com>
2011-11-04 08:15:47 -0400

    Fixed development configuration to prevent caching of asset pipeline

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,7 +11,7 @@ Zone1::Application.configure do
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = true 
+  config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
@@ -26,5 +26,5 @@ Zone1::Application.configure do
   config.assets.compress = false
 
   # Expands the lines which load the assets
-  config.assets.debug = true
+  config.assets.debug = true 
 end
