b61ad92f4a3ab62ea812410ed8d31194e3f7e053
Brian Buchalter <brianb@endpoint.com>
2011-11-28 11:53:48 -0500

    Completed flagging unit tests

--- spec/models/flagging_spec.rb
+++ spec/models/flagging_spec.rb
@@ -3,16 +3,58 @@ require 'spec_helper'
 describe Flagging do
   it { should belong_to :flag }
   it { should belong_to :stored_file }
+  it { should belong_to :user }
 
   it { should validate_presence_of :flag_id }
   it { should validate_presence_of :user_id }
 
-  context "when flagging is new" do
+  it { should allow_mass_assignment_of :flag_id }
+  it { should allow_mass_assignment_of :user_id }
+  it { should allow_mass_assignment_of :stored_file_id }
+  it { should allow_mass_assignment_of :note }
+  it { should allow_mass_assignment_of :checked }
+  
+  context "when flagging is new record" do
+    subject { Flagging.new }
     it { should_not validate_presence_of :stored_file_id }
   end
 
-  # TODO: Update this to validate presence of on update only
-  #context "when flagging is new" do
-  #  it { should validate_presence_of :stored_file_id }
-  #end
+  context "when flagging is existing record" do
+    subject { Factory(:flagging) }
+    it { should validate_presence_of :stored_file_id }
+  end
+
+  describe "#checked=" do
+    context "when called an existing record" do
+      subject { Factory(:flagging) }
+      it { should raise_error }
+    end
+    context "when passed false" do
+      before { subject.checked=false }
+      it "should return false" do 
+        subject.checked == false
+      end
+    end
+    context "when passed true" do
+      before { subject.checked=true }
+      it "should return true" do 
+        subject.checked.should == true
+      end
+    end
+  end
+
+  describe "#checked?" do
+    context "when a new record" do
+      subject { Flagging.new}
+      it "should return true" do
+        subject.checked? == true
+      end
+    end
+    context "when an existing record" do
+      subject { Factory(:flagging) }
+      it "should return false" do
+        subject.checked? == false
+      end
+    end
+  end
 end
