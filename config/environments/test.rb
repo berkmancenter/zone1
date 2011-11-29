21f0486ff807b11187381ef9ad8305ed1e64db5e
Brian Buchalter <brianb@endpoint.com>
2011-11-29 10:51:26 -0500

    Revised strategy for resetting cache after every test.  Added test to confirm it.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -6,7 +6,7 @@ Zone1::Application.configure do
   # your test database is "scratch space" for the test suite and is wiped
   # and recreated between test runs.  Don't rely on the data there!
   
-  config.cache_classes = false #avoid caching! 
+  config.cache_classes = true 
 
   # Configure static asset server for tests with Cache-Control for performance
   config.serve_static_assets = true
