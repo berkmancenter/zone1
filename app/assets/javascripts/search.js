5e69fea511b705d7fac353070c829e049011eddc
Steph Skardal <steph@endpoint.com>
2011-12-05 15:50:30 -0500

    Added toggle to list options field options. Updated to whitespace consistency.

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -8,7 +8,7 @@ $(function() {
 			ui.newContent.slideDown();
 		}
 	});
-    $('#expand a').click(function() {
+		$('#expand a').click(function() {
 		if($(this).hasClass('expanded')) {
 			$('#accordion .ui-accordion-content').slideUp();
 			$('#accordion h3').removeClass('ui-state-active').removeClass('ui-state-focus');
@@ -37,11 +37,10 @@ $(function() {
 			return false;
 		}
 		$('#results').toggleClass('list').toggleClass('grid');
-		$('.displayed').removeClass('displayed');
-		$('#display_options').hide();
+		$('.list_options').toggle();
 		$("#display_options input[type='radio']").attr("checked", false);
-    $(this).attr("checked", true);
-    return true;
+		$(this).attr("checked", true);
+		return true;
 	});
 
 	//Set options
@@ -55,35 +54,35 @@ $(function() {
 		}
 	});
 */
-  $("input[type='checkbox'].toggle_column").click(function() {
-    var selector = "span." + $(this).data("column-class");
-    if($(this).attr("checked")=="checked") {
-      $(selector).show();
-    } else {
-      $(selector).hide();
-    }
-  });
-  $('#open-tabs').submit(function() {
-    $('.downloadable:checked').each(function(index) {
-      window.open('/stored_files/'+$(this).data('stored-file-id') + '/edit');
-    });
-    return false;
-  });
+	$("input[type='checkbox'].toggle_column").click(function() {
+		var selector = "span." + $(this).data("column-class");
+		if($(this).attr("checked")=="checked") {
+			$(selector).show();
+		} else {
+			$(selector).hide();
+		}
+	});
+	$('#open-tabs').submit(function() {
+		$('.downloadable:checked').each(function(index) {
+			window.open('/stored_files/'+$(this).data('stored-file-id') + '/edit');
+		});
+		return false;
+	});
 
 	$('#download-set').submit(function() {
 		Zone1.clone_downloadable_checkboxes_to($(this));
-    return true; //submit form
+		return true; //submit form
 	});
 	$('#bulk-edit').submit(function() {
 		Zone1.clone_downloadable_checkboxes_to($(this));
-    return true; //submit form
+		return true; //submit form
 	});
 	$('#bulk-delete').submit(function() {
 		Zone1.clone_downloadable_checkboxes_to($(this));
-    var count = $(this).children("input:checked").length;
-    var confirm_delete;
-    confirm_delete = confirm("Are you sure you want to delete " + count + " items?");
-    return confirm_delete;
+		var count = $(this).children("input:checked").length;
+		var confirm_delete;
+		confirm_delete = confirm("Are you sure you want to delete " + count + " items?");
+		return confirm_delete;
 	});
 
 	$('#close_quick_edit').live("click", function() {
@@ -92,12 +91,12 @@ $(function() {
 });
 
 if(!window.Zone1) {
-  //Initializes namespace for functions
-  Zone1 = {};
+	//Initializes namespace for functions
+	Zone1 = {};
 }
 
 Zone1.clone_downloadable_checkboxes_to = function(destination) {
-  destination.children("input:checked").remove();
-  destination.append($('.downloadable:checked').clone());
-  destination.children("input:checked").hide();
+	destination.children("input:checked").remove();
+	destination.append($('.downloadable:checked').clone());
+	destination.children("input:checked").hide();
 };
