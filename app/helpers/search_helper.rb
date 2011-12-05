7a788e6aba1dd1c5d05c4fa30c2326c335459d77
Steph Skardal <steph@endpoint.com>
2011-12-05 11:54:27 -0500

    Sortable styling. Flag styling and functionality.

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -6,4 +6,27 @@ module SearchHelper
     direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
     link_to title, params.merge(:sort_column => column, :sort_direction => direction, :page => nil), {:class => css_class}
   end
+
+  def university_flag_display(search_hit)
+    flag_labels = Flag.label_map.invert
+    if search_hit.stored(:flag_ids)
+      if search_hit.stored(:flag_ids).include?(flag_labels["University Record"].to_i)
+        content_tag 'span', 'U', :class => 'highlighted'
+      elsif search_hit.stored(:flag_ids).include?(flag_labels["May be University Record"].to_i)
+        content_tag 'span', 'U'
+      end
+    end
+  end
+
+  # TODO: Update this logic to reflect 3 levels of preservation
+  def preserved_flag_display(search_hit)
+    flag_labels = Flag.label_map.invert
+    if search_hit.stored(:flag_ids)
+      if search_hit.stored(:flag_ids).include?(flag_labels["Preserved"].to_i)
+        content_tag 'span', 'P', :class => 'highlighted'
+      elsif search_hit.stored(:flag_ids).include?(flag_labels["Nominated for Preservation"].to_i)
+        content_tag 'span', 'P'
+      end
+    end
+  end
 end
