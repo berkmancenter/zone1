e9c8824c40f32c18f70a5631caa7669b23ed8fa3
Brian Buchalter <brianb@endpoint.com>
2011-12-06 11:06:08 -0500

    Tweaks to seeds and mime type category

--- app/models/mime_type_category.rb
+++ app/models/mime_type_category.rb
@@ -2,5 +2,7 @@ class MimeTypeCategory < ActiveRecord::Base
   has_many :mime_types
   has_many :stored_files, :through => :mime_types
 
+  attr_accessible :name
+
   validates_presence_of :name
 end
