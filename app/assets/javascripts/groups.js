a76d6c9f8d2a16221e04456fd24e44ce4f2049e8
Steph Skardal <steph@endpoint.com>
2012-01-11 17:09:17 -0500

    Fixed groups spacing.

--- app/assets/javascripts/groups.js
+++ app/assets/javascripts/groups.js
@@ -6,24 +6,24 @@ var zone_one_groups = {
 	setup_quickview: function() {
 		$('#quick_edit_panel .delete').live("click", function() {
 			$('#fail_response').slideUp();
-            $('<p>').html("Are you sure you want to delete this group?").dialog({
-                buttons: [
-                    {   
-                        text: "Yes",
-                        click: function() { 
-                            $('#quick_edit_delete').submit();
-                            $(this).dialog('close');
-                        }   
-                    },  
-                    {   
-                        text: "No",
-                        click: function() { $(this).dialog('close'); }   
-                    }   
-                ]   
-            }); 
-            return false;
+			$('<p>').html("Are you sure you want to delete this group?").dialog({
+				buttons: [
+					{   
+						text: "Yes",
+						click: function() { 
+							$('#quick_edit_delete').submit();
+							$(this).dialog('close');
+						}   
+					},  
+					{   
+						text: "No",
+						click: function() { $(this).dialog('close'); }
+					}   
+				]   
+			}); 
+			return false;
 
 			alert('deleted!');
-		});	
+		});
 	}
 };
