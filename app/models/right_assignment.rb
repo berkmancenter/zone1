e4ae39cdd385176174bbdb1338ab7b39840fb3ba
Brian Buchalter <brianb@endpoint.com>
2012-01-19 14:22:45 -0500

    Fixes 1856.  StoredFile.cached_viewable_user expires after right assignment.

--- app/models/right_assignment.rb
+++ app/models/right_assignment.rb
@@ -2,4 +2,36 @@ class RightAssignment < ActiveRecord::Base
   belongs_to :right
   belongs_to :subject, :polymorphic => true
   attr_accessible :right_id
+
+  after_create :destroy_perserved_flag_stored_files_cache
+  after_destroy :destroy_perserved_flag_stored_files_cache
+
+  private
+
+  def destroy_perserved_flag_stored_files_cache
+    if right.action == "view_preserved_flag_content"
+
+      # Must delete this cache first so when
+      # StoredFile.cached_viewable_users rebuilds
+      # it doesn't build from a stale cache
+
+      case subject_type  
+      when "User"
+        Rails.cache.delete("users-viewable-users-view_preserved_flag_content")
+      when "Group"
+        Rails.cache.delete("groups-viewable-users-view_preserved_flag_content")
+      when "Role"
+        Rails.cache.delete("roles-viewable-users-view_preserved_flag_content")
+      end
+    
+      perserved_flag_ids = Flag.preservation.collect{ |f| f.id }
+
+      # produces SQL:
+      # SELECT "stored_files".* FROM "stored_files" INNER JOIN "flaggings" ON "flaggings"."stored_file_id" = "stored_files"."id" WHERE "flaggings"."flag_id" IN (#{perserved_flag_ids})
+      # Unscope so that deleted stored files have their caches updated as well
+      StoredFile.unscoped.joins(:flaggings).where(:"flaggings.flag_id" => perserved_flag_ids).each do |stored_file|
+        StoredFile.send(:destroy_cache, stored_file)
+      end
+    end
+  end
 end
--- spec/models/right_assignment_spec.rb
+++ spec/models/right_assignment_spec.rb
@@ -4,4 +4,113 @@ describe RightAssignment do
   it { should belong_to(:right) }
   it { should belong_to(:subject) }
   it { should allow_mass_assignment_of :right_id }
-end
+
+  describe "after create,destroy" do
+    describe "destroy_perserved_flag_stored_files_cache" do
+      before do
+        @stored_file = Factory(:stored_file)
+        @perserved_stored_file = Factory(:stored_file)
+        flag = Factory(:flag, :name => "PRESERVED")
+        Factory(:flagging, :stored_file => @perserved_stored_file, :flag => flag) 
+        @user = Factory(:user)
+      end
+      
+      context "when a user is assigned a right" do
+        before do
+          @cached_viewable_users_before_right_assignment = StoredFile.cached_viewable_users(@perserved_stored_file)
+          @user.rights << Factory(:right, :action => "view_preserved_flag_content")
+        end
+        
+        it "stored files with the perserved flag set should add the user to the viewable_users cache" do
+          assert !@cached_viewable_users_before_right_assignment.include?(@user.id)
+          assert StoredFile.cached_viewable_users(@perserved_stored_file.id).include?(@user.id)
+        end
+
+        it "should not affect stored files without the perserved flag" do
+          assert !StoredFile.cached_viewable_users(@stored_file.id).include?(@user.id)
+        end
+  
+        context "after destroying that right" do
+          before do
+            @user.rights.first.destroy
+          end
+
+          it "stored files with the perserved flag set should add the user to the viewable_users cache" do
+            assert !StoredFile.cached_viewable_users(@perserved_stored_file.id).include?(@user.id)
+          end
+          
+          it "should not affect stored files without the perserved flag" do
+            assert !StoredFile.cached_viewable_users(@stored_file.id).include?(@user.id)
+          end
+        end #context after destroying that right
+      end #context when user assigns right directly
+
+      context "when a group is assigned a right" do
+
+        before do
+          @cached_viewable_users_before_right_assignment = StoredFile.cached_viewable_users(@perserved_stored_file)
+          @group = Factory(:group, :assignable_rights => true)
+          Membership.add_users_to_groups([@user], [@group])
+          @group.rights << Factory(:right, :action => "view_preserved_flag_content")
+        end
+        
+        it "stored files with the perserved flag set should add the user to the viewable_users cache" do
+          assert !@cached_viewable_users_before_right_assignment.include?(@user.id)
+          assert StoredFile.cached_viewable_users(@perserved_stored_file.id).include?(@user.id)
+        end
+
+        it "should not affect stored files without the perserved flag" do
+          assert !StoredFile.cached_viewable_users(@stored_file.id).include?(@user.id)
+        end
+  
+        context "after destroying that right" do
+          before do
+            @group.rights.first.destroy
+          end
+
+          it "stored files with the perserved flag set should add the user to the viewable_users cache" do
+            assert !StoredFile.cached_viewable_users(@perserved_stored_file.id).include?(@user.id)
+          end
+          
+          it "should not affect stored files without the perserved flag" do
+            assert !StoredFile.cached_viewable_users(@stored_file.id).include?(@user.id)
+          end
+        end #context after destroying that right
+      end #context group assignment
+
+      context "when a role is assigned a right" do
+
+        before do
+          @cached_viewable_users_before_right_assignment = StoredFile.cached_viewable_users(@perserved_stored_file)
+          @role = Factory(:role)
+          @user.roles << @role
+          @role.rights << Factory(:right, :action => "view_preserved_flag_content")
+        end
+        
+        it "stored files with the perserved flag set should add the user to the viewable_users cache" do
+          assert !@cached_viewable_users_before_right_assignment.include?(@user.id)
+          assert StoredFile.cached_viewable_users(@perserved_stored_file.id).include?(@user.id)
+        end
+
+        it "should not affect stored files without the perserved flag" do
+          assert !StoredFile.cached_viewable_users(@stored_file.id).include?(@user.id)
+        end
+  
+        context "after destroying that right" do
+          before do
+            @role.rights.first.destroy
+          end
+
+          it "stored files with the perserved flag set should add the user to the viewable_users cache" do
+            assert !StoredFile.cached_viewable_users(@perserved_stored_file.id).include?(@user.id)
+          end
+          
+          it "should not affect stored files without the perserved flag" do
+            assert !StoredFile.cached_viewable_users(@stored_file.id).include?(@user.id)
+          end
+        end #context after destroying that right
+      end #context role assignment
+
+    end #describe destroy_perserved_flag
+  end #describe after create,estroy
+end #dewscribe Right Assignment
