39a5c890680960ea952d236c4a63e4f209f65545
Steph Skardal <steph@endpoint.com>
2012-09-18 14:24:05 -0400

    Reindex stored files when MimeType category changes.

--- app/controllers/admin/mime_types_controller.rb
+++ app/controllers/admin/mime_types_controller.rb
@@ -5,12 +5,14 @@ class Admin::MimeTypesController < Admin::BaseController
 
   def update
     begin
-      MimeType.find(params[:id]).update_attributes!(params[:mime_type])
+      mime_type = MimeType.find(params[:id])
+      mime_type.update_attributes!(params[:mime_type])
+      mime_type.stored_files.each(&:index)
+      Sunspot.commit
       render :json => "Successfully updated!"
     rescue Exception => e
       render :json => e.to_s, :status => :error
       log_exception e
     end
   end
- 
 end
